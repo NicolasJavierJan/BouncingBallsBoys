8b2e1847f3681eafb74ae14e85ee2eae579d63cb
andreaDiClaudio <andreadicla@gmail.com>
2022-04-08 12:14:50 +0200

    Ball class added

---
+++ src/main/java/com/example/bouncingballsboys/Ball.java
@@ -0,0 +1,23 @@
+package com.example.bouncingballsboys;
+
+public class Ball {
+
+    private double X;
+    private double Y;
+
+    public double getX() {
+        return X;
+    }
+
+    public void setX(double x) {
+        X = x;
+    }
+
+    public double getY() {
+        return Y;
+    }
+
+    public void setY(double y) {
+        Y = y;
+    }
+}
