b53fb0b4bab5ed2d84eee40831e82543d0fbf2ff
NicolasJavierJan <nicolasjavierjan@gmail.com>
2022-04-10 17:22:42 +0200

    Collision Control

--- src/main/java/com/example/bouncingballsboys/BallController.java
+++ src/main/java/com/example/bouncingballsboys/BallController.java
@@ -45,6 +45,17 @@ public class BallController implements Initializable {
                 if (bottomBorder || upperBorder) {
                     deltaBlueY *= -1;
                 }
+
+                // collision detection:
+                double dx = blueBall.getLayoutX() - purpleBall.getLayoutX();
+                double dy = blueBall.getLayoutY() - purpleBall.getLayoutY();
+                double distance = Math.sqrt(dx * dx + dy * dy);
+
+                if (distance < blueBall.getRadius() + purpleBall.getRadius()){
+                    deltaBlueX *= -1;
+                    deltaBlueY *= -1;
+                }
+
             }
         }));
         timelineBlue.setCycleCount(Animation.INDEFINITE);
@@ -52,17 +63,16 @@ public class BallController implements Initializable {
     }
 
     public void movePBall() {
-
-        Ball pBall = new Ball();
-        pBall.setX(2);
-        pBall.setY(2);
         Timeline timelinePurple = new Timeline(new KeyFrame(Duration.millis(10), new EventHandler<ActionEvent>() {
+
+            double deltaPurpleX = 1;
+            double deltaPurpleY = 1;
 
 
             @Override
             public void handle(ActionEvent actionEvent) {
-                purpleBall.setLayoutX(purpleBall.getLayoutX() + pBall.getX());
-                purpleBall.setLayoutY(purpleBall.getLayoutY() + pBall.getY());
+                purpleBall.setLayoutX(purpleBall.getLayoutX() + deltaPurpleX);
+                purpleBall.setLayoutY(purpleBall.getLayoutY() + deltaPurpleY);
                 Bounds bounds = anchorPane.getBoundsInLocal();
 
                 boolean rightBorder = purpleBall.getLayoutX() >= (bounds.getMaxX() - purpleBall.getRadius());
@@ -70,63 +80,32 @@ public class BallController implements Initializable {
                 boolean bottomBorder = purpleBall.getLayoutY() >= (bounds.getMaxY() - purpleBall.getRadius());
                 boolean upperBorder = purpleBall.getLayoutY() <= (bounds.getMinY() + purpleBall.getRadius());
 
-
-//
-//                // checking balls by X
-//                boolean axisX = (purpleBall.getLayoutX()+purpleBall.getRadius()) == 100;
-//                // checking balls by Y
-//                boolean axisY = (purpleBall.getLayoutY()+purpleBall.getRadius()) == 100;
-
-
-
                 if (rightBorder || leftBorder) {
-                    pBall.setX(pBall.getX()*(-1));
+                    deltaPurpleX *= -1;
                 }
                 if (bottomBorder || upperBorder) {
-                    pBall.setY(pBall.getY()*(-1));
+                    deltaPurpleY *= -1;
                 }
 
+                double dx = blueBall.getLayoutX() - purpleBall.getLayoutX();
+                double dy = blueBall.getLayoutY() - purpleBall.getLayoutY();
+                double distance = Math.sqrt(dx * dx + dy * dy);
 
-//                if(axisY)
-//                {
-//                    deltaPurpleY *=-1;
-//                    System.out.println("I do exist");
-//                }
+                if (distance < blueBall.getRadius() + purpleBall.getRadius()){
+                    deltaPurpleX *= -1;
+                    deltaPurpleY *= -1;
+                }
             }
 
         }));
         timelinePurple.setCycleCount(Animation.INDEFINITE);
         timelinePurple.play();
-        System.out.println(blueBall.getLayoutX());
-        System.out.println(blueBall.getLayoutY());
-
-        // checking balls by X
-        boolean axisX1 = (purpleBall.getLayoutX()+purpleBall.getRadius()) == (blueBall.getRadius() + blueBall.getLayoutX());
-        boolean axisX2 = (purpleBall.getLayoutX()-purpleBall.getRadius()) == (blueBall.getRadius() + blueBall.getLayoutX());
-        boolean axisX3 = (purpleBall.getLayoutX()+purpleBall.getRadius()) == (blueBall.getRadius() - blueBall.getLayoutX());
-        boolean axisX4 = (purpleBall.getLayoutX()-purpleBall.getRadius()) == (blueBall.getRadius() - blueBall.getLayoutX());
-        // checking balls by Y
-        boolean axisY1 = (purpleBall.getLayoutY()+purpleBall.getRadius()) == (blueBall.getRadius() + blueBall.getLayoutY());
-        boolean axisY2 = (purpleBall.getLayoutY()-purpleBall.getRadius()) == (blueBall.getRadius() + blueBall.getLayoutY());
-        boolean axisY3 = (purpleBall.getLayoutY()+purpleBall.getRadius()) == (blueBall.getRadius() - blueBall.getLayoutY());
-        boolean axisY4 = (purpleBall.getLayoutY()-purpleBall.getRadius()) == (blueBall.getRadius() - blueBall.getLayoutY());
-
-        if(axisX1 && axisY1 || axisX1 && axisY2 || axisX1 && axisY3 || axisX1 && axisY4
-                || axisX2 && axisY1 || axisX2 && axisY2 || axisX2 && axisY3 || axisX2 && axisY4
-                || axisX3 && axisY1 || axisX3 && axisY2 || axisX3 && axisY3 || axisX3 && axisY4
-                || axisX4 && axisY1 || axisX4 && axisY2 || axisX4 && axisY3 || axisX4 && axisY4 )
-        {
-            pBall.setX(pBall.getX()*(-1));
-            pBall.setY(pBall.getY()*(-1));
-            System.out.println("I do exist");
-        }
     }
 
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle){
-        while(true)
-        {movePBall();}
-        //moveBBall();
+        moveBBall();
+        movePBall();
 
     }
 
